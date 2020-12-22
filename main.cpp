780f93d86cabc898b3451dd07010135b7b5335e9
plnpln <15306554218@163.com>
2020-12-22 18:53:54 +0800

    add my test file

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "clion->github" << endl;
+    cout << "github->aliyun" << endl;
+    cout << "aliyun&clion" << endl;
+    return 0;
+}
